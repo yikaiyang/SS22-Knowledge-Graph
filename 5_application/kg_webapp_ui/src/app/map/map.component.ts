116faae9caec197f942ca4436454246273ec988f
Yikai Yang <yikai.yang@outlook.com>
2023-08-28 18:48:07 +0200

    Updated frontend with sidemenu

--- 5_application/kg_webapp_ui/src/app/map/map.component.ts
+++ 5_application/kg_webapp_ui/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from './map.service';
+import { Observable as BehaviourSubject, Subject, of } from 'rxjs';
+import { Location } from '../app/responses/location';
 
 @Component({
   selector: 'app-map',
@@ -8,14 +10,69 @@ import { MapService } from './map.service';
 })
 export class MapComponent implements OnInit {
 
+  models = [
+    'TransE',
+    'TransD',
+    'TransH',
+    'RotatE',
+  ]
 
-  locations: any[] = []
+  tab_menu_options = ['Existing', 'Predictions']
 
-  constructor(private mapService: MapService) {
-    this.locations = this.mapService.locations;
+  selected_tab_menu_option = this.tab_menu_options[0]
+
+  onSelectTabMenuItem() {
+    if (this.selected_tab_menu_option == this.tab_menu_options[0]) {
+
+    } else if (this.selected_tab_menu_option == this.tab_menu_options[1]) {
+      
+    }
+  }
+
+  relationships = [
+    'IS_CONNECTED',
+    'IS_LOCATED',
+    'IS_NEARBY',
+    'HAS_INCIDENT',
+    'HAS_TEMPERATURE',
+    'HAS_WEATHER',
+    'HAS_TRAFFIC_SITUATION'
+  ]
+
+  selectedRelationship = '';
+
+  selectedLocation$ = new Subject<Location>();
+
+  locations$: BehaviourSubject<Location[]> = of(
+    [
+    {
+      "node_id": "5cf54e0d8af84867a651f58079be05f5",
+      "latitude": 48.216403322294354,
+      "longitude": 16.341783329844475,
+      "length": 41,
+      "name": "Innerer Gürtel-41"
+    }
+  ]
+  )
+
+  constructor(
+    private mapService: MapService
+  ) {
   }
 
   ngOnInit(): void {
+    this.locations$ = this.mapService.locations$;
   }
 
+
+  onItemClick(location: Location) {
+    this.display = true;
+    this.selectedLocation$.next(location);
+  }
+
+  display: boolean = true;
+
+  showDialog() {
+    this.display = true;
+  }
 }
